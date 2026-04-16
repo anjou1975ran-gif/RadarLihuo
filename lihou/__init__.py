afd751cf8a2c87904c290c43c8bec7271c28ce36
AnjouRan <anjou1975ran@gmail.com>
2026-04-16 08:39:41 +0800

    Update __init__.py

--- lihou/__init__.py
+++ lihou/__init__.py
@@ -1,3 +1,15 @@
-from lihuo import run
-# lihuo/__init__.py
-from .core import 
+"""Public package exports for lihou."""
+
+from .core import run
+
+__all__ = ["run"]
+
+"""Backward-compatible package alias.
+
+This allows imports such as `from lihuo import run` while the canonical
+implementation currently lives in the `lihou` package directory.
+"""
+
+from lihou import run
+
+__all__ = ["run"]
